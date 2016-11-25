c768bbeefb9edf9ff6173b08471d1cba093d9d7f
Kosegi <SK040122@main.intgin.net>
2016-11-25 16:49:35 +0100

    desctructured

--- app.jsx
+++ app.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 // import {createStore} from 'redux';
 import axios from 'axios';
 
-function CommentList(comments) {
+
+// prop is object need to by destructured
+const CommentList = function({arr}) {
   const renderComment = function({body, author}, index) {
     return <li key={index.toString()}>{body}-{author}</li>
   }
-  // console.log(comments)
-  return (
-    <ul> {comments.map(renderComment)}</ul>
-  )
+  console.log(arr)
+  return <ul> {arr.map(renderComment)}</ul>;
+
 }
 
 const renderComment = function({body, author}, index) {
@@ -30,10 +31,8 @@ class AppContainer extends React.Component {
   }
 
   render() {
-    const coms = this.state.comments;
-    console.log(coms);
-    return <ul> {this.state.comments.map(renderComment)} </ul>
-    // return <CommentList comments={this.state.comments}/>
+    // return <ul> {this.state.comments.map(renderComment)} </ul>
+    return <CommentList arr={this.state.comments}/>
   }
 
 }
